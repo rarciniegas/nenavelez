65efc1f943cb650c034906e1bc9f49400ab393d3
Roberto Arciniegas <rarci003@fiu.edu>
2024-03-22 01:17:41 -0400

    first commit

---
+++ pages/home.php
@@ -0,0 +1,12 @@
+<?php include 'header.php'; ?>
+<main>
+    <div class="px-4 py-5 my-5 text-center">
+        <img class="d-block mx-auto mb-4" src="../images/rose.png" alt="" width="72" height="75
+        ">
+        <h1 class="display-5 fw-bold text-body-emphasis">Nena Velez</h1>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">Beautiful floral arrangements for every occasion.</p>
+        </div>
+    </div>
+</main>
+<?php include 'footer.php'; ?>
